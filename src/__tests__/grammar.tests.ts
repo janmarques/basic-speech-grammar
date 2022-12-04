a99ea7055bf6152a5c727f7c0e678d64f4d6ca71
Jan Marques <jan@janmarques.net>
2022-12-04 15:05:43 +0100

    asd

--- src/__tests__/grammar.tests.ts
+++ src/__tests__/grammar.tests.ts
@@ -5,9 +5,46 @@ const speechResultOngoing = {
     resultIndex: 0
 } as unknown as any;
 
-const haystack = ["blue", "red", "GREEN", "pur ple"];
+interface ResultList {
+    [name: number]: Result;
+    isFinal: boolean;
+}
+class Result {
+    constructor(public transcript: string) { }
+}
+
+
+const spokenWordList1 = { isFinal: true } as ResultList;
+spokenWordList1[0] = new Result("blue")
+spokenWordList1[1] = new Result("below")
+spokenWordList1[2] = new Result("black")
+
+const spokenWordList2 = ["person", "woman", "man", "camera", "tv"]
+const spokenWordList3 = ["blue", "red", "black", "white"]
+const spokenWordList4 = ["very", "stable", "genious"]
+const speechResultFinishedMatch = {
+    results: [spokenWordList1],
+    resultIndex: 0
+} as any;
+
+const haystack = ["blue", "red", "GREEN", "sta ble"];
 
 test('Ongoing yields no result', () => {
     var result = tryFindCommand(speechResultOngoing, haystack);
+    console.log("lala1")
+    console.log(result.conflictingResults)
+    console.log(result.result)
+    console.log(result.resultType)
+    console.log("lili1")
+    expect(ResultType.RecognitionNotFinished).toBe(result.resultType);
+});
+
+test('Match', () => {
+    var result = tryFindCommand(speechResultFinishedMatch, haystack);
+    console.log("lala2")
+    console.log(result.conflictingResults)
+    console.log(result.result)
+    console.log(result.resultType)
+    console.log("lili2")
     expect(ResultType.RecognitionNotFinished).toBe(result.resultType);
 });
